7da8a8da40b16b21a07aa2725bcd3fc341777600
Ian Martin <ian@imartin.net>
2024-05-22 18:13:35 -0600

    add makefile

---
+++ Makefile
@@ -0,0 +1,69 @@
+PROJECT_NAME := nextride-shortcut
+PKG := "$(PROJECT_NAME)"
+CMD := server.go
+GO_FILES := $(shell find . -type f -name '*.go' | grep -v _test.go)
+OS := $(shell go env GOOS)
+ARCH := $(shell go env GOARCH)
+PROJECT_NAME := $(PROJECT_NAME)-$(OS)-$(ARCH)
+
+.SUFFIXES:
+.PHONY: all coverage lint test race x2unit xunit clean download locallint
+
+all: $(PROJECT_NAME) $(PROJECT_NAME).sha256
+
+ifeq ($(GITHUB_ACTIONS),)
+coverage: html
+lint: locallint
+else
+coverage: coverage.out
+lint: cilint.txt
+endif
+
+ifdef GITHUB_HEAD_REF
+LDFLAGS := -ldflags="-X main.Version=$(GITHUB_HEAD_REF)"
+endif
+
+locallint:
+	@golangci-lint run
+
+cilint.txt: $(GO_FILES)
+	@golangci-lint run --out-format=line-number --new-from-rev=master --issues-exit-code=0 > $@
+
+test:
+	@go test -short ./...
+
+race:
+	@go test -v -count=1 -race ./...
+
+coverage.out: $(GO_FILES)
+	@go test -v -race -covermode=atomic -coverprofile=$@ ./...
+
+html: coverage.out
+	@go tool cover --html=$<
+
+x2unit:
+	go get github.com/tebeka/go2xunit
+
+tests.out:
+	@go test -v -race ./... > $@
+
+xunit: x2unit tests.out
+	go2xunit -fail -input tests.out -output tests.xml
+	@rm -f tests.out
+
+run:
+	@go run $(CMD) --verbosity debug
+
+clean:
+	@rm -rf tests.xml tests.out coverage.out *.sha256 main cilint.txt $(PKG) $(PKG)-*
+	@docker rm --force $(PKG) &> /dev/null || true
+	@go mod tidy
+
+$(PROJECT_NAME): $(GO_FILES)
+	@go build -v -o $@ $(LDFLAGS) $(CMD)
+
+%.sha256:
+	@openssl dgst -sha256 -hex $* | cut -f2 -d' ' > $@
+
+download: 
+	@go mod download
---
+++ server.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"flag"
+	stdlog "log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/aztechian/nextride-shortcut/internal/server"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
+	"github.com/rs/zerolog/log"
+)
+
+var Version = "dev"
+
+func main() {
+	var listen, level string
+	var proxy bool
+	flag.StringVar(&level, "verbosity", "info", "log level")
+	flag.StringVar(&level, "v", "info", "log level")
+	flag.StringVar(&listen, "listen", ":8080", "address to listen on")
+	flag.StringVar(&listen, "l", ":8080", "address to listen on")
+	flag.BoolVar(&proxy, "proxy", false, "running behind a reverse proxy")
+
+	flag.Parse()
+	setupLogging(level)
+	server := setupServer(listen, proxy)
+
+	go startServer(server)
+
+	// Setting up signal capturing
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	// Waiting for SIGINT (kill -2)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("Error while shutting down server")
+	}
+}
+
+func setupLogging(userLevel string) {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	if level, err := zerolog.ParseLevel(userLevel); err != nil {
+		zerolog.SetGlobalLevel(level)
+	}
+	if pid := os.Getpid(); pid == 1 {
+		log.Logger = zerolog.New(os.Stderr).With().Timestamp().Logger() // json text logger when in a container
+	} else {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+}
+
+func setupServer(addr string, useProxy bool) *http.Server {
+	commonMiddleware := []server.Middleware{
+		hlog.NewHandler(log.Logger), //just inject the logger in the context here
+		hlog.RequestIDHandler("request_id", "Request-Id"),
+	}
+	if useProxy {
+		// proxy must come before RemoteIPHandler, if its being used
+		commonMiddleware = append(commonMiddleware, server.ProxyMiddleware)
+	}
+	commonMiddleware = append(commonMiddleware,
+		hlog.RemoteIPHandler("remote_ip"),
+		hlog.MethodHandler("method"),
+		hlog.HostHandler("host", true),
+		hlog.URLHandler("url"),
+		hlog.UserAgentHandler("user_agent"),
+		server.LoggerMiddleware,
+		server.SecurityMiddleware,
+		server.HeaderMiddleware,
+	)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", server.RedirectHandler) // direct requests to root to /next
+	mux.HandleFunc("/next", server.NextHandler)
+	return &http.Server{Addr: addr, Handler: server.WrapHandler(mux, commonMiddleware...), ErrorLog: stdlog.New(log.Logger, "", 0)}
+}
+
+func startServer(server *http.Server) {
+	// Start the HTTP server on port 8080
+	log.Info().Str("addr", server.Addr).Str("version", Version).Msg("starting server")
+	// fmt.Println("Server listening on port 8080...")
+	log.Fatal().
+		Err(server.ListenAndServe()).
+		Msg("closing server")
+}
